5bf6f6f71b41f3d0239329f88eefea0847f33fcb
Bum-Seok Hwang <bitofsky@naver.com>
2017-03-08 23:50:28 +0900

    update

---
+++ ajst.js
@@ -0,0 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./ajst/autocollect"));
+__export(require("./ajst/core"));
+__export(require("./ajst/option"));
+__export(require("./ajst/prepare"));
+__export(require("./ajst/template"));
+__export(require("./ajst/tplCompiler"));
+//# sourceMappingURL=ajst.js.map
